a69168b20f029d6bb259b2f5853afc420322430c
Florian <38866797+floz30@users.noreply.github.com>
2024-11-04 19:05:58 +0100

    Disable EdgeToEdge and add two new functions

--- app/src/main/java/fr/floz/epicurian/MainActivity.kt
+++ app/src/main/java/fr/floz/epicurian/MainActivity.kt
@@ -1,9 +1,14 @@
 package fr.floz.epicurian
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import fr.floz.epicurian.ui.MainContent
 
@@ -12,7 +17,7 @@ import fr.floz.epicurian.ui.MainContent
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
 
         super.onCreate(savedInstanceState)
 
@@ -20,4 +25,22 @@ class MainActivity : ComponentActivity() {
             MainContent()
         }
     }
+
+
+}
+
+fun Context.getActivity(): Activity? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    return null
+}
+
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
 }
