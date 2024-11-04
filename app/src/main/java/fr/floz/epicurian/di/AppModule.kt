55e6f223e221969a3114c903847c125fa7c8302d
Florian <38866797+floz30@users.noreply.github.com>
2024-11-04 19:01:48 +0100

    Update di layer

--- app/src/main/java/fr/floz/epicurian/di/AppModule.kt
+++ app/src/main/java/fr/floz/epicurian/di/AppModule.kt
@@ -8,9 +8,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import fr.floz.epicurian.data.AppDatabase
-import fr.floz.epicurian.data.RestaurantDao
-import fr.floz.epicurian.data.RestaurantsRepository
-import fr.floz.epicurian.data.RestaurantsRepositoryImpl
+import fr.floz.epicurian.data.implementation.ElementsRepositoryImpl
+import fr.floz.epicurian.data.services.local.ElementDao
+import fr.floz.epicurian.data.services.remote.OverpassApi
+import fr.floz.epicurian.data.services.remote.OverpassApiImpl
+import fr.floz.epicurian.domain.repo.ElementsRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -21,19 +27,42 @@ object AppModule {
     @Singleton
     fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
         return Room
-            .databaseBuilder(context, AppDatabase::class.java, "restaurants.db")
+            .databaseBuilder(context, AppDatabase::class.java, "epicurean.db")
+            .createFromAsset("database/epicurean.db")
+            // Supprime les données présentes en bdd lors d'une mise à jour de version
+            // https://medium.com/androiddevelopers/understanding-migrations-with-room-f01e04b07929
+            .fallbackToDestructiveMigration()
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideRestaurantDao(database: AppDatabase): RestaurantDao {
-        return database.restaurantDao()
+    fun provideRestaurantDao(database: AppDatabase): ElementDao {
+        return database.elementDao()
     }
 
     @Provides
     @Singleton
-    fun provideRestaurantRepository(restaurantDao: RestaurantDao): RestaurantsRepository {
-        return RestaurantsRepositoryImpl(restaurantDao)
+    fun provideRestaurantRepository(elementDao: ElementDao): ElementsRepository {
+        return ElementsRepositoryImpl(elementDao)
     }
+
+    @Provides
+    @Singleton
+    fun provideOsm(client: HttpClient): OverpassApi {
+        return OverpassApiImpl(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+
 }
